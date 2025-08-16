4b10853be1d6e932e1b7262392c99ac056ae3301
ViniciusKaique <viniciusloma@gmail.com>
2025-08-16 15:26:40 +0000

    ok

--- reverse_bit.c
+++ reverse_bit.c
@@ -1,1 +1,38 @@
+#include <stdio.h>
 
+unsigned char reverse_bit(unsigned char n)
+{
+    unsigned char rev = 0;
+    int i = 8;
+    while(i >= 0)
+    {
+        rev = rev*2 + (n%2);
+        n = n / 2;
+        i--;
+    }
+    return (rev);
+}
+
+int main (void)
+{
+    unsigned char n = 42, x = 0;
+    int i = 7;
+
+    printf("initial decimal: %u bit: ", n);
+    while(i != 0)
+    {
+        printf("%d", n >> i & 1);
+        i--;
+    }
+    printf("\n");
+    x = reverse_bit(n);
+    i = 7;
+    printf("decimal reverse bit: %u bit: ", x);
+    while(i != 0)
+    {
+        printf("%u", x >> i & 1);
+        i--;
+    }
+        printf("\n");
+
+}
